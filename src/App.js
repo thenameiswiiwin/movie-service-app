762718cbd0b034f80eb203eafd105601846757a9
Huy Nguyen <huyn.nguyen95@gmail.com>
2021-08-30 20:42:24 -0500

    movie component, tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Movie from './components/Movie';
+
+const URL = 'https://yts-proxy.now.sh/list_movies.json?sort_by=rating';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+
+	async function getMovies() {
+		const {
+			data: {
+				data: { movies },
+			},
+		} = await axios.get(URL);
+		setIsLoading(false);
+		setMovies(movies);
+	}
+
+	useEffect(() => {
+		getMovies();
+	});
+
+	return (
+		<section className="container">
+			{isLoading ? (
+				<div className="loader">
+					<span className="loader__Text">Loading...</span>
+				</div>
+			) : (
+				<div className="movies">
+					{movies.map((movie) => (
+						<Movie
+							key={movie.id}
+							id={movie.id}
+							year={movie.year}
+							title={movie.title}
+							summary={movie.summary}
+							poster={movie.medium_cover_image}
+							genres={movie.genres}
+						/>
+					))}
+				</div>
+			)}
+		</section>
+	);
 }
 
 export default App;
